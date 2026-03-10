78b254fab31ac431629aa47f56ee9ddeaa9d0f75
Sanskruti Gagare <sanskrutigagre65@gmail.com>
2026-03-11 01:17:53 +0530

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,66 @@
 
-console.log("This from js file")
+const resultElement = document.getElementById("results");
+let recognition;
+
+
+  function startConverting(){
+      if('webkitSpeechRecognition' in window){
+        recognition = new webkitSpeechRecognition();
+        setupRecoginition(recognition);
+        recognition.start();
+      }
+  }
+
+
+  function setupRecoginition(recognition){
+
+    recognition.continuous = true;
+
+    recognition.interimResult = true;
+
+    recognition.lang = 'en-US';
+
+    recognition.onresults = function(event){
+
+        / / processResult()
+
+
+      processResult(event.results);
+
+    }
+
+  }
+  
+
+  function processResult(results){
+
+
+    let finalTransmit = '';
+    let interTransmit = '';
+
+    for (let i = 0; i < results.length; i++){
+
+
+      let transcript = results[i][0].transcript;
+      transcript.replace("\n","<br>");
+
+      if(results[i].isFinal){
+
+        finalTranscript += transcript
+      }
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+  function stopConverting(){
+
+  }
